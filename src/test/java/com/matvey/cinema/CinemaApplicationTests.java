83395ee632faf3b174503876b4e3b84a0359a1d6
Матвей Василюк <matveyvasiluk@Noutbuk-Matvej.local>
2025-02-17 03:12:46 +0300

    1 lab

--- src/test/java/com/matvey/cinema/CinemaApplicationTests.java
+++ src/test/java/com/matvey/cinema/CinemaApplicationTests.java
@@ -3,8 +3,6 @@ package com.matvey.cinema;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,10 +15,6 @@ class CinemaApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Test
-    void contextLoads() {
-    }
 
     @Test
     void testGetWithQueryParams() throws Exception {
